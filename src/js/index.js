12d83fa6d333ed3e5ae4538075b55989265dbe49
Prog <gypsy@openmailbox.org>
2018-04-30 16:47:22 +0300

    delete some fonts, change adjustment, delete bootstrap, update normalize

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+/**
+ * Application entry point
+ */
+
+// Load application styles
+import '../css/main.scss';
+
+// ================================
+// START YOUR APP HERE
+// ================================
+
+console.log('Webpack 4 is sweet');
